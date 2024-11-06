c0a0ee37ac6a50516f6311f1b785206d8180689b
ghktndyd <hwasuyong99@naver.com>
2024-11-06 12:51:40 +0900

    feat(ConvenienceStore, View): 입/출력을 담당하는 클래스 생성

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,11 @@
 package store;
 
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConvenienceStore convenienceStore = new ConvenienceStore(new InputView(), new OutputView());
+        convenienceStore.run();
     }
 }
---
+++ src/main/java/store/ConvenienceStore.java
@@ -0,0 +1,18 @@
+package store;
+
+import store.view.InputView;
+import store.view.OutputView;
+
+public class ConvenienceStore {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ConvenienceStore(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+    }
+}
