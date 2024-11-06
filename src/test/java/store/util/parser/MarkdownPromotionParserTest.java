be58092ab577363797938e06fd3d37aee0cf717d
ghktndyd <hwasuyong99@naver.com>
2024-11-06 16:56:03 +0900

    test(MarkdownPromotionParserTest): 발생할 수 있는 예외 상황 테스트
    - 정상 케이스에서는 예외가 발생하지 않는다.
    - 파일 헤더가 정해진 형식과 다르다면 예외가 발생한다.
    - 숫자여야 하는 컬럼이 숫자가 아니라면 예외가 발생한다.
    - 날짜여야 하는 컬럼이 정해진 날짜 형식이 아니라면 예외가 발생한다.

---
+++ src/test/java/store/util/parser/MarkdownPromotionParserTest.java
@@ -0,0 +1,90 @@
+package store.util.parser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.exception.CustomException;
+import store.exception.ExceptionMessage;
+import store.promotion.domain.Promotion;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MarkdownPromotionParserTest {
+
+    private MarkdownPromotionParser parser;
+
+    @BeforeEach
+    void setUp() {
+        parser = new MarkdownPromotionParser("src/test/resources/promotions.md");
+    }
+
+    @DisplayName("모든 값이 정상이라면 promotions.md 파일이 정상적으로 파싱된다.")
+    @Test
+    void parseValidPromotion() {
+        String validFileContents = """
+                name,buy,get,start_date,end_date
+                탄산2+1,2,1,2024-01-01,2024-12-31
+                """;
+
+        List<Promotion> promotions = parser.parse(new BufferedReader(new StringReader(validFileContents)));
+
+        assertThat(promotions).hasSize(1);
+        assertThat(promotions.getFirst()).isEqualTo(
+                new Promotion("탄산2+1", 2, 1, LocalDate.of(2024, 1, 1), LocalDate.of(2024, 12, 31))
+        );
+    }
+
+    @DisplayName("헤더 형식이 잘못 되었다면 예외가 발생한다.")
+    @Test
+    void parseInvalidHeader() {
+        String invalidHeader = """
+                test,buy,get,start_date,end_date
+                탄산2+1,2,1,2024-01-01,2024-12-31
+                """;
+
+        assertThatThrownBy(() -> parser.parse(new BufferedReader(new StringReader(invalidHeader))))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining(ExceptionMessage.INVALID_HEADER.getMessage());
+    }
+
+    @DisplayName("수량 형식이 잘못 되었다면 예외가 발생한다.")
+    @Test
+    void parseInvalidQuantity() {
+        String invalidQuantity = """
+                name,buy,get,start_date,end_date
+                탄산2+1,test,1,2024-01-01,2024-12-31
+                """;
+
+        assertThatThrownBy(() -> parser.parse(new BufferedReader(new StringReader(invalidQuantity))))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining(ExceptionMessage.INVALID_NUMBER_FORMAT.getMessage());
+    }
+
+    @DisplayName("날짜 형식이 잘못 되었다면 예외 발생한다.")
+    @Test
+    void parseInvalidDate() {
+        String invalidDate = """
+                name,buy,get,start_date,end_date
+                탄산2+1,2,1,test,2024-12-31
+                """;
+
+        assertThatThrownBy(() -> parser.parse(new BufferedReader(new StringReader(invalidDate))))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining(ExceptionMessage.INVALID_DATE_FORMAT.getMessage());
+    }
+
+    @DisplayName("파일이 비어 있다면 예외가 발생한다.")
+    @Test
+    void parseEmptyFile() {
+        String emptyContent = "";
+
+        assertThatThrownBy(() -> parser.parse(new BufferedReader(new StringReader(emptyContent))))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining(ExceptionMessage.EMPTY_FILE.getMessage());
+    }
+}
