e6563555d8d70f4eee48e416a29f301b8f3dcc36
ghktndyd <hwasuyong99@naver.com>
2024-11-10 11:16:32 +0900

    feat(OrderItem): 프로모션이 적용 기능 구현

--- src/main/java/store/order/OrderItem.java
+++ src/main/java/store/order/OrderItem.java
@@ -1,20 +1,41 @@
 package store.order;
 
+import store.promotion.domain.Promotion;
+
 public class OrderItem {
 
     private final String productName;
-    private final int quantity;
+    private final int buyQuantity;
+    private int freeQuantity = 0;
+    private int promotionDiscount = 0;
 
-    public OrderItem(String productName, int quantity) {
+    public OrderItem(String productName, int buyQuantity) {
         this.productName = productName;
-        this.quantity = quantity;
+        this.buyQuantity = buyQuantity;
     }
 
     public String getProductName() {
         return productName;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public int getBuyQuantity() {
+        return buyQuantity;
+    }
+
+    public void applyPromotion(Promotion promotion, int applicableStock, int productPrice) {
+        int promotionApplyCount = buyQuantity / promotion.getBuyQuantity();
+
+        freeQuantity = promotionApplyCount * promotion.getFreeQuantity();
+
+        int totalQuantity = buyQuantity + freeQuantity;
+
+        if (totalQuantity > applicableStock) {
+            freeQuantity = applicableStock - buyQuantity;
+            if (freeQuantity < 0) {
+                freeQuantity = 0;
+            }
+        }
+
+        promotionDiscount = freeQuantity * productPrice;
     }
 }
